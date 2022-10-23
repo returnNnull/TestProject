eae002ce8f5c23b781ec59ef72ab1285cf24ab31
betir <betirov95@gmail.com>
2022-10-23 13:48:12 +0300

    logger

--- app/src/main/java/com/bam/testproject/MainActivity.kt
+++ app/src/main/java/com/bam/testproject/MainActivity.kt
@@ -1,54 +1,88 @@
 package com.bam.testproject
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var emailText: EditText
-    lateinit var passText: EditText
-    lateinit var loginBtn: TextView
-    lateinit var people: People
+    var LOG_TAG = "MYLOG"
+    var BUNGLE_COUNT_KEY = "count_value"
 
-
-    override fun onStart() {
-        super.onStart()
-        people = People("dfsf", 45)
-    }
+    lateinit var textView: TextView
+    lateinit var btn: Button
+    var count: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.screen2)
-        initForms()
+        setContentView(R.layout.sec)
+        Log.d(LOG_TAG, "onCreate() called")
+        initViews()
 
-        loginBtn.setOnClickListener{
-
-            val login = emailText.text.toString()
-            val pass = passText.text.toString()
-
-            if (login == "admin" && pass == "12345"){
-                showNotification("Success")
-            }
-            else{
-                showNotification("Error")
-            }
+        if (savedInstanceState != null){
+            count = savedInstanceState.getInt(BUNGLE_COUNT_KEY)
+            textView.text = count.toString()
         }
 
+        btn.setOnClickListener {
+            count++
+            textView.text = count.toString()
+            Log.i(LOG_TAG, count.toString())
+        }
+
+
     }
 
-
-    fun showNotification(text: String){
-        Toast.makeText(baseContext, text, Toast.LENGTH_SHORT).show()
+    private fun initViews() {
+        textView = findViewById(R.id.textView)
+        btn = findViewById(R.id.btn)
     }
 
-
-    fun initForms() {
-        emailText = findViewById(R.id.emailEditText)
-        passText = findViewById(R.id.passwordEditText)
-        loginBtn = findViewById(R.id.loginButton)
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart() called")
+        Log.i(LOG_TAG, count.toString())
     }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LOG_TAG, "onRestart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy() called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(LOG_TAG, "onSaveInstanceState() called")
+        outState.putInt(BUNGLE_COUNT_KEY, count)
+        Log.d(LOG_TAG, "count value saved")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(LOG_TAG, "onRestoreInstanceState() called")
+    }
+
 }
 
