48381a23784c8f2d40b50f4f44c5ee4f779f23bb
betir <betirov95@gmail.com>
2022-10-23 17:30:54 +0300

    intent!!!

--- app/src/main/java/com/bam/testproject/LoginActivity.kt
+++ app/src/main/java/com/bam/testproject/LoginActivity.kt
@@ -1,28 +1,74 @@
 package com.bam.testproject
 
+import android.Manifest
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var signUpBtn: TextView
+    lateinit var message: EditText
+    lateinit var hour: EditText
+    lateinit var min: EditText
+    lateinit var btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        signUpBtn = findViewById(R.id.sign_up_btn)
+        initViews()
 
-        signUpBtn.setOnClickListener {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            intent.putExtra(LOGIN_TO_REGISTRATION_KEY, "This message from login")
-            startActivity(intent)
+        val contact = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            var adress: Uri? = it.data?.data
+            var proj: Array<String> = arrayOf(CommonDataKinds.Phone.DISPLAY_NAME)
+            var cursor: Cursor? = contentResolver.query(adress!!, proj, null,
+                null, null)
+
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    val index = cursor.getColumnIndex(CommonDataKinds.Phone.DISPLAY_NAME)
+                    val name = cursor.getString(index)
+                    message.setText(name)
+                }
+            }
         }
 
 
+        val permission = registerForActivityResult(RequestPermission()) {
+            if (it) {
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type = CommonDataKinds.Phone.CONTENT_TYPE
+                contact.launch(intent)
+
+            } else {
+                Toast.makeText(this, "denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+        btn.setOnClickListener {
+            permission.launch(Manifest.permission.READ_CONTACTS)
+        }
+
+    }
+
+
+    private fun initViews() {
+        message = findViewById(R.id.message_text_view)
+        hour = findViewById(R.id.hour_text_view)
+        min = findViewById(R.id.min_text_view)
+        btn = findViewById(R.id.send_btn)
     }
 }
 
